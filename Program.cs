4e07f7398bb41a42c7d403da7002363b68142f8c
Your Name <you@example.com>
2022-11-01 17:59:53 +0400

    программа на нахождение чётных чисел в массиве. ДЗ №1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void PrintArray (int[] arr)
+{   int count = arr.Length;
+    int pos=0;
+    while (pos<count)
+        {Console.Write($"{arr[pos]} ");
+        pos++;}
+}
+void FillArr (int[] array)
+{   int length = array.Length;
+        for (int i=0; i<length; i++){
+        array[i] = new Random().Next(100,999);}
+} 
+
+Console.WriteLine("Введите число элементов массива");
+int len = int.Parse(Console.ReadLine());
+
+int[] massiv = new int [len];
+FillArr(massiv);
+PrintArray(massiv);
+int kol=0;
+for (int j=0; j<massiv.Length; j++){
+    if (massiv[j]%2==0) kol++;
+}
+Console.WriteLine();
+Console.WriteLine("количество чётных чисел = "+kol);
+
+
+
+
