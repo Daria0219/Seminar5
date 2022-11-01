f7c83837998cc403dc3ee1c544b140a16e6b9491
Your Name <you@example.com>
2022-11-01 18:15:31 +0400

    сумма элементов, стоящих на нечётных позициях. ДЗ №2

--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
 ﻿
 void PrintArray (int[] arr)
-{   int count = arr.Length;
-    int pos=0;
-    while (pos<count)
-        {Console.Write($"{arr[pos]} ");
-        pos++;}
+{   for (int pos=0; pos<arr.Length; pos++)
+    {Console.Write($"{arr[pos]} ");}
 }
 void FillArr (int[] array)
 {   int length = array.Length;
         for (int i=0; i<length; i++){
-        array[i] = new Random().Next(100,999);}
+        array[i] = new Random().Next(-100,100);}
 } 
 
 Console.WriteLine("Введите число элементов массива");
@@ -19,14 +16,14 @@ int[] massiv = new int [len];
 FillArr(massiv);
 PrintArray(massiv);
 
-int KolichestvoChetnih(int[] array){
-int kol=0;
-for (int j=0; j<array.Length; j++){
-    if (array[j]%2==0) kol++;}
-return kol;
+int SumNaNechetPos(int[] array){
+int sum=0;
+for (int j=1; j<array.Length; j=j+2){
+    sum=sum+array[j];}
+return sum;
 }
 Console.WriteLine();
-Console.WriteLine("количество чётных чисел = "+KolichestvoChetnih(massiv));
+Console.WriteLine("сумма элементов, стоящих на нечётных позициях = "+SumNaNechetPos(massiv));
 
 
 
