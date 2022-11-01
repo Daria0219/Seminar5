c3380834d736124218df8370fff674cf128ee77c
Your Name <you@example.com>
2022-11-01 18:32:57 +0400

    разница между максимальным и минимальным элементом массива. ДЗ №3

--- Program.cs
+++ Program.cs
@@ -1,29 +1,30 @@
 ﻿
-void PrintArray (int[] arr)
+void PrintArray (double[] arr)
 {   for (int pos=0; pos<arr.Length; pos++)
     {Console.Write($"{arr[pos]} ");}
 }
-void FillArr (int[] array)
+void FillArr (double[] array)
 {   int length = array.Length;
         for (int i=0; i<length; i++){
-        array[i] = new Random().Next(-100,100);}
+        array[i] = new Random().Next(0,100);}
 } 
 
 Console.WriteLine("Введите число элементов массива");
 int len = int.Parse(Console.ReadLine());
 
-int[] massiv = new int [len];
+double[] massiv = new double [len];
 FillArr(massiv);
 PrintArray(massiv);
 
-int SumNaNechetPos(int[] array){
-int sum=0;
-for (int j=1; j<array.Length; j=j+2){
-    sum=sum+array[j];}
-return sum;
-}
+double max=massiv[0];
+double min=massiv[0];
+for (int j=1; j<massiv.Length; j++){
+    if (massiv[j]>max) max=massiv[j];
+    if (massiv[j]<min) min=massiv[j];}
+double Razniza=(max-min);
+
 Console.WriteLine();
-Console.WriteLine("сумма элементов, стоящих на нечётных позициях = "+SumNaNechetPos(massiv));
+Console.WriteLine("разница между максимальным и минимальным элементом массива = "+Razniza);
 
 
 
